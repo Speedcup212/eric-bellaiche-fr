b880e57a49ceaac719b7ad760b81d1aa0174529c
Speedcup212 <eric.bellaiche@gmail.com>
2026-08-21 15:39:57 +0200

    Autoriser le payload JSON dynamique du recueil

--- eslint.config.js
+++ eslint.config.js
@@ -24,5 +24,11 @@ export default tseslint.config(
         { allowConstantExport: true },
       ],
     },
+  },
+  {
+    files: ['src/pages/portal/ClientRecueilJourneyPage.tsx'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   }
 );
