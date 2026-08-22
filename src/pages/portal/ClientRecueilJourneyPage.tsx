d9c3dd565a39db81c8a5852f529cc13e14ce641e
Speedcup212 <eric.bellaiche@gmail.com>
2026-08-22 09:44:48 +0200

    Supprimer les mutations DOM du Recueil

--- src/pages/portal/ClientRecueilJourneyPage.tsx
+++ src/pages/portal/ClientRecueilJourneyPage.tsx
@@ -2,8 +2,6 @@ import { useEffect, useState } from 'react';
 import { CheckCircle2, Pencil } from 'lucide-react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import ClientRecueilJourneyBase from './ClientRecueilJourneyBase';
-import RecueilUxEnhancements from './RecueilUxEnhancements';
-import RecueilValidationVisuals from './RecueilValidationVisuals';
 import { JourneyProgress, PageIntro, WizardCard } from '../../portal/FintechJourney';
 import { supabase } from '../../lib/supabase';
 import { dossierHref, fetchPortalProgress, messageFromError, selectedProgress, type PortalProgress } from '../../portal/portalHelpers';
@@ -67,5 +65,5 @@ export default function ClientRecueilJourneyPage() {
     </div>;
   }
 
-  return <><RecueilUxEnhancements /><RecueilValidationVisuals /><ClientRecueilJourneyBase /></>;
+  return <ClientRecueilJourneyBase />;
 }
