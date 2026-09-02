ba2dad2444e3fd2659bc4daba3e247d21e5918fc
Speedcup212 <eric.bellaiche@gmail.com>
2026-09-02 08:42:21 +0200

    Remove global DOM observer from portal runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import { installRecueilValidationUx } from './portal/recueilValidationUx';
 import './index.css';
 import './patrimony-dark.css';
 import './documents-contrast-fix.css';
 import './cabinet-dark.css';
 import './recueil-validation.css';
-
-installRecueilValidationUx();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
