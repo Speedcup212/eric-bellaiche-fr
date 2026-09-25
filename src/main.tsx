c70bc269925f64c244fcb320a5c949bb2fe43ca3
Speedcup212 <eric.bellaiche@gmail.com>
2026-09-25 08:23:20 +0200

    perf: keep portal-only assets out of public entry bundle

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './patrimony-dark.css';
-import './documents-contrast-fix.css';
-import './cabinet-dark.css';
-import './recueil-validation.css';
-import './recueil-validation';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
